80cb0ca883347486e20fc6057b94dcfcd6363604
LimiO <52657506+LimiO@users.noreply.github.com>
2019-09-13 19:48:11 +0300

    Update config.py

--- config/config.py
+++ config/config.py
@@ -3,7 +3,7 @@ TOKEN = '829333255:AAH7B6hqC6IAaNqaw6b0QhylKx_sNq7Styc'
 def autosending_text(bot, message):
 	first_name = bot.get_chat(message.chat.id).first_name
 	text = """Hello, {0}
-This is the starter template for other bots built with python. 
+This is the starter template for other bots built with <b>python</b>. 
 
 No bots cooked so far.
 
