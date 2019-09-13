6b583a9bfa3d2a6cc524bbbcf1040a445567835f
LimiO <52657506+LimiO@users.noreply.github.com>
2019-09-13 19:44:52 +0300

    Update config.py

--- config/config.py
+++ config/config.py
@@ -3,13 +3,10 @@ TOKEN = '829333255:AAH7B6hqC6IAaNqaw6b0QhylKx_sNq7Styc'
 def autosending_text(bot, message):
 	first_name = bot.get_chat(message.chat.id).first_name
 	text = """Hello, {0}
-This is the starter template for other bots built with <b>node-telegram-bot-api</b>. Visit my website vadimcpp.ru to see bots built on this template.
+This is the starter template for other bots built with python. 
 
-Bots cooked:
-@gdgcensorbot;
-@horoshoposidelibot;
-@fxswap_bot;
-@gdgdevbot;
+No bots cooked so far.
+
 
 Enjoy cooking with <a href = 'https://github.com/LimiO/TutorialHeroku'>this template</a>!""".format(first_name)
 	return text
