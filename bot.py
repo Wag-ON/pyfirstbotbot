f8cb6b4d812f03c0dfd3c32d8bccac3741ea88bb
Wag-ON <51058872+Wag-ON@users.noreply.github.com>
2019-12-24 20:43:56 +0200

    Add files via upload

--- bot.py
+++ bot.py
@@ -137,4 +137,4 @@ if __name__ == '__main__':
     else:
 		# Сервер необходим для работы webhook
         server = init_server()
-        server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
+        server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 8000)))
